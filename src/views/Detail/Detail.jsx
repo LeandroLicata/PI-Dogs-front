c576494fd3c6fecb29518b8e6cd7b7529095311f
LeandroLicata <leandrolicata1@gmail.com>
2023-02-13 09:48:32 -0300

    detail mas mejor

--- src/views/Detail/Detail.jsx
+++ src/views/Detail/Detail.jsx
@@ -26,25 +26,27 @@ export default function Detail(props) {
           src={loadingImage}
           alt="Loading..."
           width="300px"
-          style={{ margin: "auto", verticalAlign: 'middle' }}
+          style={{ margin: "auto", verticalAlign: "middle" }}
         />
       )}
-      {!isLoading && (
-        <div>
-          <h1 className="detail_name">{myDog.name}</h1>
-          <img src={myDog.image || dogImage} alt="not found" width="400px" />
-          <p className="detail_text">
-            Temperaments: {myDog.temperament?.join(", ")}
-          </p>
-          <p className="detail_text">{`Weight: ${
-            myDog.weight && myDog.weight[0]
-          } - ${myDog.weight && myDog.weight[1]} Kg`}</p>
-          <p className="detail_text">{`Height: ${
-            myDog.height && myDog.height[0]
-          } - ${myDog.height && myDog.height[1]} cm`}</p>
-          <p className="detail_text">{`Life Span: ${myDog.life_span}`}</p>
-        </div>
-      )}
+      <div className="detail_div">
+        {!isLoading && (
+          <div>
+            <h1 className="detail_name">{myDog.name}</h1>
+            <img src={myDog.image || dogImage} alt="not found" width="400px" />
+            <p className="detail_text">
+              Temperaments: {myDog.temperament?.join(", ")}
+            </p>
+            <p className="detail_text">{`Weight: ${
+              myDog.weight && myDog.weight[0]
+            } - ${myDog.weight && myDog.weight[1]} Kg`}</p>
+            <p className="detail_text">{`Height: ${
+              myDog.height && myDog.height[0]
+            } - ${myDog.height && myDog.height[1]} cm`}</p>
+            <p className="detail_text">{`Life Span: ${myDog.life_span}`}</p>
+          </div>
+        )}
+      </div>
       <Link to="/home">
         <button className="return_button">Return</button>
       </Link>
