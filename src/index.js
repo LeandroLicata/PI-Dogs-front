768a28b203e8f50e314e9436a69db4f313e64b80
LeandroLicata <leandrolicata1@gmail.com>
2023-03-07 19:25:21 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from "./redux/store";
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:3001";
-axios.defaults.baseURL = "https://pi-dogs-back-production-8f16.up.railway.app/";
+axios.defaults.baseURL = "https://pi-dogs-back-5k5l.onrender.com";
 
 ReactDOM.render(
   <Provider store={store}>
